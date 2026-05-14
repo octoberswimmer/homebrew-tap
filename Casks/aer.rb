f91a9f7dec74e4de02e5fd50712b47a5bb04d816
Christian G. Warden <cwarden@xerus.org>
2026-05-14 17:13:33 -0500

    Add aer Cask

---
+++ Casks/aer.rb
@@ -0,0 +1,19 @@
+cask "aer" do
+  arch arm: "arm64", intel: "amd64"
+  os macos: "darwin", linux: "linux"
+
+  version "1.0.0-beta.2"
+  sha256 arm:          "8534ad88f718462caa2f487c4b9d83faf38cb2c3fa1ca151646ccbd8cfa28996",
+         x86_64:       "36ae3b7dcd562052483ff63a739e2e5f9402b3f9f5c6abaf1f74d39bad3941e4",
+         arm64_linux:  "43bbeb550a03c12c3507aa5196042fc46e0628192a8171917bfb9731e2fda1cf",
+         x86_64_linux: "e257612ff228d741ddc7026815bebbcc11357888905d286499cef89a08926642"
+
+  url "https://github.com/octoberswimmer/aer-dist/releases/download/v#{version}/aer_#{os}_#{arch}_v#{version}.zip"
+  name "aer"
+  desc "Apex Execution Runtime"
+  homepage "https://www.octoberswimmer.com/tools/aer/"
+
+  auto_updates true
+
+  binary "aer"
+end
--- README.md
+++ README.md
@@ -1,18 +1,14 @@
-# Octoberswimmer Tap
+# October Swimmer Tap
 
-## How do I install these formulae?
+## How do I install these casks?
 
-`brew install octoberswimmer/tap/<formula>`
+`brew install octoberswimmer/tap/<cask>`
 
-Or `brew tap octoberswimmer/tap` and then `brew install <formula>`.
+Or `brew tap octoberswimmer/tap` and then `brew install <cask>`.
 
 Or, in a `brew bundle` `Brewfile`:
 
 ```ruby
 tap "octoberswimmer/tap"
-brew "<formula>"
+brew "<cask>"
 ```
-
-## Documentation
-
-`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
