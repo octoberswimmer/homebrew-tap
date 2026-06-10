a0e646e52f1d1f95759e625296227858a8ec561e
Christian G. Warden <cwarden@xerus.org>
2026-06-10 18:06:11 -0500

    aer 1.0.11

--- Casks/aer.rb
+++ Casks/aer.rb
@@ -2,11 +2,11 @@ cask "aer" do
   arch arm: "arm64", intel: "amd64"
   os macos: "darwin", linux: "linux"
 
-  version "1.0.10"
-  sha256 arm:          "4f86589b57b4f019bc11a6f01b5cacfbd15d254c5398480e3570d0b0949e1b96",
-         x86_64:       "bad24e8594176a3f5f6fd10d1e5eb913299a331a21835f008df24c55b7786233",
-         arm64_linux:  "6b83c556bc50a07935ed218165566bebc226a7c91e0468612776a7b160407ee8",
-         x86_64_linux: "650c9b0c6595ed015c5980a447b88517ffd3bc4b76700b2d9481c07d6251e968"
+  version "1.0.11"
+  sha256 arm:          "ca5a0096138d397d6f9b510827f340716ad6eb8692e1862b6543eb9f78ebfb80",
+         x86_64:       "d5c4f35754dbc4b35375df95fb6e25b07a0bbc983225574f4b1c91c88b9f9d5b",
+         arm64_linux:  "5993377d755e93f186448cddd6f62377d43fcb698b449642def9eb9551b580e4",
+         x86_64_linux: "dfe016ebd00d47615bb9d80cc6de7ff041f04ef7526c34150e84173cc6f922b9"
 
   url "https://github.com/octoberswimmer/aer-dist/releases/download/v#{version}/aer_#{os}_#{arch}_v#{version}.zip"
   name "aer"
