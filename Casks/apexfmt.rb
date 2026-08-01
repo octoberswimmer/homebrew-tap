56920e29bafc3bcb117d2fec3e69026e5f52e391
Christian G. Warden <cwarden@xerus.org>
2026-08-01 11:45:19 -0500

    apexfmt 0.60.0

--- Casks/apexfmt.rb
+++ Casks/apexfmt.rb
@@ -2,11 +2,11 @@ cask "apexfmt" do
   arch arm: "arm64", intel: "amd64"
   os macos: "darwin", linux: "linux"
 
-  version "0.59.0"
-  sha256 arm:          "850a509ff0d728b47331c403fd247076102fae70874283ad0a3d2419cc1721e9",
-         x86_64:       "935c1bb899f1c52c125fc11dd4ba15f52a389f1bccdc4245817f6ee7c1bc9430",
-         arm64_linux:  "157e27401314147ad8e02c618597fe47561d7e3c2a438e846ef1545e567c877c",
-         x86_64_linux: "d709e309c2d92c3d21615ebbf692bea90341f98aba7182ca5877eb85067b37e0"
+  version "0.60.0"
+  sha256 arm:          "45343ea6674511c0690e2a5e662d810b738fe1ead8f5c4f2764bc9db09b96115",
+         x86_64:       "71531b0677dc2f96bf83929659262ad8aeeb332544da33784cf3379023c8fb46",
+         arm64_linux:  "42c8edbd6e40e6725ae97053840c32ac964238f16b2346c725af0edc4f2683cd",
+         x86_64_linux: "0ac147715751fb3866df68f549ba7c3afdfae65e50fc32780a017e0414231b46"
 
   url "https://github.com/octoberswimmer/apexfmt/releases/download/v#{version}/apexfmt_#{os}_#{arch}_v#{version}.zip",
       verified: "github.com/octoberswimmer/apexfmt/"
